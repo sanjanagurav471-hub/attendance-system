1324a477e2355d6a34881e11a8b16f98ff5cb4c7
Sanjana <sanjanagurav471@gmail.com>
2026-05-10 21:24:44 +0530

    vercel deployment

--- app.py
+++ app.py
@@ -64,7 +64,6 @@ def create_app(config_name=None):
     
     return app
 
-    app = create_app()
-
+app = create_app()
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True)
